91b1705c64ce2732a6c91969eb732862a73a1170
mparajdi@stud.uni-obuda.hu <mparajdi@stud.uni-obuda.hu>
2025-03-16 12:11:12 +0100

    Added: Sensor class

---
+++ src/AutomatedCar/SystemComponents/Sensors/Sensor.cs
@@ -0,0 +1,21 @@
+﻿namespace AutomatedCar.SystemComponents.Sensors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using AutomatedCar.Models;
+
+    class Sensor : SystemComponent
+    {
+        public Sensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+        }
+
+        public override void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
