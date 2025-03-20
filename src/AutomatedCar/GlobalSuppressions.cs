a9ea20f8658ae6d8093b1893ecbddf7ca8681fcd
bence-hoffmann <80154328+bence-hoffmann@users.noreply.github.com>
2025-03-20 21:57:08 +0100

    Update StyleCop suppressions for file headers
    Removed existing suppressions for SA1633 in the namespaces
    `AutomatedCar.Models` and `AutomatedCar.SystemComponents.Packets`.
    Replaced them with new entries justifying the absence of file
    headers as "No copyright or license." Added additional
    suppressions for `AutomatedCar.SystemComponents.Enums` and
    a general suppression without a specific target.

--- src/AutomatedCar/GlobalSuppressions.cs
+++ src/AutomatedCar/GlobalSuppressions.cs
@@ -5,5 +5,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "<Pending>", Scope = "namespace", Target = "~N:AutomatedCar.Models")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "<Pending>", Scope = "namespace", Target = "~N:AutomatedCar.SystemComponents.Packets")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "No copyrigth or license", Scope = "namespace", Target = "~N:AutomatedCar.Models")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "No copyrigth or license", Scope = "namespace", Target = "~N:AutomatedCar.SystemComponents.Packets")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "No copyrigth or license", Scope = "namespace", Target = "~N:AutomatedCar.SystemComponents.Enums")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "No copyrigth or license")]
