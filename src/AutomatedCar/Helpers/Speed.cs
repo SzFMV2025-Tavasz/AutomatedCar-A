9d1e4e8a5b269b3f418f90300b02b1f152d5895b
Kristóf Göncző <kristofgonczo@gmail.com>
2025-03-27 17:03:08 +0100

    Adding skeleton of Speed

---
+++ src/AutomatedCar/Helpers/Speed.cs
@@ -0,0 +1,42 @@
+﻿namespace AutomatedCar.Helpers
+{
+    internal class Speed
+    {
+        private double PixelsPerTick { get; set; }
+
+        private Speed(double pixelsPerTick)
+        {
+            this.PixelsPerTick = pixelsPerTick;
+        }
+
+        public static Speed FromKmPerHour(double kmPerHour)
+        {
+            return new Speed(kmPerHour);
+        }
+
+        public static Speed FromMetersPerSecond(double metersPerSecond)
+        {
+            return new Speed(metersPerSecond);
+        }
+
+        public static Speed FromPixelsPerTick(double pixelsPerTick)
+        {
+            return new Speed(pixelsPerTick);
+        }
+
+        public double InKmPerHour()
+        {
+            return PixelsPerTick;
+        }
+
+        public double InMetersPerSecond()
+        {
+            return PixelsPerTick;
+        }
+
+        public double InPixelsPerTick()
+        {
+            return PixelsPerTick;
+        }
+    }
+}
