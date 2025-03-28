a9f7ec63230f3be7bad20bedaefb52a305e66fec
Kristóf Göncző <60845715+KristofGonczo@users.noreply.github.com>
2025-03-28 09:30:32 +0100

    Speed adatstruktúra skeletonja (#43)
    * Adding skeleton of Speed
    
    * Adding TODO comments
    
    * Calculating a meter in pixels
    
    * Implementing Speed.FromMetersPerSecond
    
    * Fixing Speed.FromMetersPerSecond
    
    * Implementing Speed.FromKmPerHour

---
+++ src/AutomatedCar/Helpers/Speed.cs
@@ -0,0 +1,50 @@
+﻿namespace AutomatedCar.Helpers
+{
+    internal class Speed
+    {
+        // Calculated by comparing the average road widh in Hungary to the road_2lane_straight.png road width ~= 49.69
+        // Calculated by comparing the Skoda Octavia length to the car_1_white.png height ~= 51.09
+        public const double MeterToPixels = 50;
+
+        private double PixelsPerTick { get; set; }
+
+        private Speed(double pixelsPerTick)
+        {
+            this.PixelsPerTick = pixelsPerTick;
+        }
+
+        public static Speed FromKmPerHour(double kmPerHour)
+        {
+            return FromMetersPerSecond(kmPerHour / 3.6);
+        }
+
+        public static Speed FromMetersPerSecond(double metersPerSecond)
+        {
+            double pixelsPerSecond = metersPerSecond * MeterToPixels;
+            double pixelsPerTick = pixelsPerSecond / GameBase.TicksPerSecond;
+            return FromPixelsPerTick(pixelsPerTick);
+        }
+
+        public static Speed FromPixelsPerTick(double pixelsPerTick)
+        {
+            return new Speed(pixelsPerTick);
+        }
+
+        public double InKmPerHour()
+        {
+            // TODO Implement unit conversion
+            return PixelsPerTick;
+        }
+
+        public double InMetersPerSecond()
+        {
+            // TODO Implement unit conversion
+            return PixelsPerTick;
+        }
+
+        public double InPixelsPerTick()
+        {
+            return PixelsPerTick;
+        }
+    }
+}
