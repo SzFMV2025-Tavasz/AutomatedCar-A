5c3c8301855257ad9323f5d9df1100dd7bccf1ed
mparajdi@stud.uni-obuda.hu <mparajdi@stud.uni-obuda.hu>
2025-04-06 15:54:17 +0200

    Object detection

--- src/AutomatedCar/SystemComponents/Sensors/Radar.cs
+++ src/AutomatedCar/SystemComponents/Sensors/Radar.cs
@@ -6,10 +6,13 @@
     using System.Numerics;
     using System.Text;
     using System.Threading.Tasks;
+    using AutomatedCar.Helpers;
     using AutomatedCar.Models;
 
     class Radar : SystemComponent
     {
+        AutomatedCar car;
+        TriangleDetector triangle;
         int offsetLength;
         Vector2 dir;
         Vector2 anchor;
@@ -23,7 +26,16 @@
         }
         public override void Process()
         {
-            //TODO: object detection
+            var collidableObjects = triangle
+                .ScanVisibleObjects(
+                    World.Instance.WorldObjects,
+                    (car.X, car.Y),
+                    ((int)Position.X,
+                    (int)Position.Y),
+                    car.Rotation)
+                .Where(
+                    obj => obj.Collideable);
+
         }
         double CorrectRotation(double rotation)
         {
@@ -35,18 +47,20 @@
         }
         public Radar(VirtualFunctionBus virtualFunctionBus, AutomatedCar car, int offset) : base(virtualFunctionBus)
         {
+            triangle = new TriangleDetector(200);
+            this.car = car;
             this.offsetLength = offset;
             this.anchor = new Vector2(car.X, car.Y);
             this.dir = new Vector2((float)Math.Cos(CorrectRotation(car.Rotation) * (Math.PI/180)), (float)Math.Sin(CorrectRotation(car.Rotation) * (Math.PI / 180)));
-            car.PropertyChangedEvent += (s, e) =>
+            this.car.PropertyChangedEvent += (s, e) =>
             {
                 switch (e.PropertyName)
                 {
                     case "X":
-                        anchor.X = car.X;
+                        anchor.X = this.car.X;
                         break;
                     case "Y":
-                        anchor.Y = car.Y;
+                        anchor.Y = this.car.Y;
                         break;
                     case "Rotation":
                         dir.X = (float)Math.Cos(CorrectRotation(car.Rotation) * (Math.PI / 180));
