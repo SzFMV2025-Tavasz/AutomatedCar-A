b777199cb71e3afa2570f156c27b45623e00b883
mparajdi@stud.uni-obuda.hu <mparajdi@stud.uni-obuda.hu>
2025-04-06 17:38:43 +0200

    Radar process

--- src/AutomatedCar/SystemComponents/Sensors/Radar.cs
+++ src/AutomatedCar/SystemComponents/Sensors/Radar.cs
@@ -8,9 +8,11 @@
     using System.Threading.Tasks;
     using AutomatedCar.Helpers;
     using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
 
     class Radar : SystemComponent
     {
+        VirtualFunctionBus bus;
         AutomatedCar car;
         TriangleDetector triangle;
         int offsetLength;
@@ -26,16 +28,46 @@
         }
         public override void Process()
         {
+            bus.RadarPackets = new List<IReadOnlyRadarPacket>();
+
             var collidableObjects = triangle
                 .ScanVisibleObjects(
                     World.Instance.WorldObjects,
                     (car.X, car.Y),
-                    ((int)Position.X,
-                    (int)Position.Y),
+                    ((int)Position.X, (int)Position.Y),
                     car.Rotation)
                 .Where(
-                    obj => obj.Collideable);
+                    obj => obj.Collideable)
+                .ToList();
 
+            collidableObjects.ForEach(obj =>
+            {
+                var vecToObj = new Vector2(obj.X, obj.Y) - Position;
+                var objRotInRadians = CorrectRotation(obj.Rotation) * (Math.PI / 180);
+                var objDirVec = new Vector2((float)Math.Cos(objRotInRadians), (float)Math.Sin(objRotInRadians));
+                bus.RadarPackets.Add(new RadarPacket()
+                {
+                    Angle = Math.Atan2(vecToObj.Y, vecToObj.X),
+                    Distance = vecToObj.Length(),
+                    RelativeVelocity = (dir * car.Speed) - (objDirVec * GetObjectSpeed(obj)),
+                    Type = obj.WorldObjectType
+                });
+            });
+        }
+        static float GetObjectSpeed(WorldObject obj)
+        {
+            if (obj is Car car)
+            {
+                return car.Speed;
+            }
+            else if (obj is Npc npc)
+            {
+                return (float)Speed.FromKmPerHour(npc.Speed).InPixelsPerTick();
+            }
+            else
+            {
+                return 0;
+            }
         }
         double CorrectRotation(double rotation)
         {
@@ -47,8 +79,9 @@
         }
         public Radar(VirtualFunctionBus virtualFunctionBus, AutomatedCar car, int offset) : base(virtualFunctionBus)
         {
-            triangle = new TriangleDetector(200);
+            triangle = new TriangleDetector(200 * (int)Speed.MeterToPixels);
             this.car = car;
+            this.bus = virtualFunctionBus;
             this.offsetLength = offset;
             this.anchor = new Vector2(car.X, car.Y);
             this.dir = new Vector2((float)Math.Cos(CorrectRotation(car.Rotation) * (Math.PI/180)), (float)Math.Sin(CorrectRotation(car.Rotation) * (Math.PI / 180)));
