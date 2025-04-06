c21f1894c445185fa1d499ddf026f8d3bcf9f758
mparajdi@stud.uni-obuda.hu <mparajdi@stud.uni-obuda.hu>
2025-04-06 16:10:51 +0200

    Added: Radar packets

---
+++ src/AutomatedCar/SystemComponents/Packets/IReadOnlyRadarPacket.cs
@@ -0,0 +1,12 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using AutomatedCar.Models;
+
+    public interface IReadOnlyRadarPacket
+    {
+        double Angle { get; }
+        double Distance { get; }
+        double RelativeSpeed { get; }
+        WorldObjectType Type { get; }
+    }
+}
---
+++ src/AutomatedCar/SystemComponents/Packets/RadarPacket.cs
@@ -0,0 +1,32 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using AutomatedCar.Models;
+    using ReactiveUI;
+    public class RadarPacket : ReactiveObject, IReadOnlyRadarPacket
+    {
+        double angle;
+        double distance;
+        double relativeSpeed;
+        WorldObjectType type;
+        public double Angle
+        {
+            get => this.angle;
+            set => this.RaiseAndSetIfChanged(ref this.angle, value);
+        }
+        public double Distance
+        {
+            get => this.distance;
+            set => this.RaiseAndSetIfChanged(ref this.distance, value);
+        }
+        public double RelativeSpeed
+        {
+            get => this.relativeSpeed;
+            set => this.RaiseAndSetIfChanged(ref this.relativeSpeed, value);
+        }
+        public WorldObjectType Type
+        {
+            get => this.type;
+            set => this.RaiseAndSetIfChanged(ref this.type, value);
+        }
+    }
+}
