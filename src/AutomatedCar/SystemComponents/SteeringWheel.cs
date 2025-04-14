5cfa78241a3795e0e0f6ae778c11aee23da8304a
Kristóf Göncző <kristofgonczo@gmail.com>
2025-04-14 18:36:08 +0200

    Implementing SteeringWheel SystemComponent

---
+++ src/AutomatedCar/SystemComponents/SteeringWheel.cs
@@ -0,0 +1,31 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using System;
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+
+    internal class SteeringWheel : SystemComponent
+    {
+        public SteeringWheelPacket SteeringWheelPacket { get; }
+
+        public SteeringWheel(VirtualFunctionBus virtualFunctionBus)
+            : base(virtualFunctionBus)
+        {
+            this.SteeringWheelPacket = new SteeringWheelPacket();
+            this.virtualFunctionBus.SteeringWheelPacket = this.SteeringWheelPacket;
+        }
+
+        public override void Process()
+        {
+            if (Keyboard.IsKeyDown(Avalonia.Input.Key.Left))
+            {
+                this.SteeringWheelPacket.State = Math.Max(-450, this.SteeringWheelPacket.State - 1);
+            }
+
+            if (Keyboard.IsKeyDown(Avalonia.Input.Key.Right))
+            {
+                this.SteeringWheelPacket.State = Math.Min(450, this.SteeringWheelPacket.State + 1);
+            }
+        }
+    }
+}
