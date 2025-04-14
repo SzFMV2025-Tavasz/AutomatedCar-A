2c7b7b52349db96b8a0adf18be9e6709352b7bbc
Kristóf Göncző <kristofgonczo@gmail.com>
2025-04-14 18:35:51 +0200

    Creating SteeringWheel packets

---
+++ src/AutomatedCar/SystemComponents/Packets/IReadOnlySteeringWheelPacket.cs
@@ -0,0 +1,16 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IReadOnlySteeringWheelPacket
+    {
+        /// <summary>
+        /// The state of the steering wheel. A number between -450 and 450, where 0 is the straight state.
+        /// </summary>
+        int State { get; }
+    }
+}
---
+++ src/AutomatedCar/SystemComponents/Packets/SteeringWheelPacket.cs
@@ -0,0 +1,20 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using ReactiveUI;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class SteeringWheelPacket : ReactiveObject, IReadOnlySteeringWheelPacket
+    {
+        private int state = 0;
+
+        public int State
+        {
+            get => this.state;
+            set => this.RaiseAndSetIfChanged(ref this.state, value);
+        }
+    }
+}
